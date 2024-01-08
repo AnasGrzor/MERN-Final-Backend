5af39463f590c99c0d47b5e353e32af66f025398
AnasGrzor <anaskhalid658@gmail.com>
2024-01-09 01:23:51 +0500

    10

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({
-  origin: "https://wizapp.vercel.app/",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true
-}));
+app.use(cors)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
