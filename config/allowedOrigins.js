65ad209641f815481d80bead83ab601c626809e0
AnasGrzor <anaskhalid658@gmail.com>
2024-01-08 23:36:40 +0500

    BUGGGGGGGGG

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://wombat-golden-skunk.ngrok-free.app",
+  "https://wizapp.vercel.app/"
 ];
 
 module.exports = allowedOrigins;
