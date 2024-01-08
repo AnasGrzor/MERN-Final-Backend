d2293394d9f7029d782b03b332131420aa7061f4
AnasGrzor <anaskhalid658@gmail.com>
2024-01-08 23:57:53 +0500

    1

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 4000;
 
+app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+      );
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+      }
+      next();
+    });
+
 app.use(cors());
 
 app.use(express.json());
